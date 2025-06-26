74627aa46653973be0049d7fd2c4bb4da7e9e7ca
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-06-26 22:17:33 +0400

    fix for next lecture prepare

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Select, SelectValue, SelectTrigger } from "@/components/ui/select";
-
 export default function Home() {
   return (
     <div>
-      <Button size={"xs"}>Click me</Button>
-      <Button variant={"secondary"}>Secondary</Button>
-      <Button variant={"destructive"} size={"lg"}>
-        Destructive
-      </Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"muted"}>Muted</Button>
-      <Button variant={"teritary"}>Teritary</Button>
-      <Input />
-      <Select>
-        <SelectTrigger>
-          <SelectValue placeholder="Select an option" />
-        </SelectTrigger>
-      </Select>
+      <h1>we will continue next lecture</h1>
     </div>
   );
 }
